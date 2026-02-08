6074bbcffc3a34da5952fe1d2387c2d33a20e161
Suhieee <jhonloydluzares35@gmail.com>
2026-02-08 20:07:35 +0800

    Fix STATIC_ROOT configuration for Railway deployment

--- smart_dorm_finder/settings.py
+++ smart_dorm_finder/settings.py
@@ -210,11 +210,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files configuration - Use Cloudinary in production, local in development
 CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME', '')
